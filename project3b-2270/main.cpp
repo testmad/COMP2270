4eeefa26656781153daf93e366df6f6d9533d59b
testmad <testmad@thinstack.net>
2015-11-12 01:44:48 -0500

    i hate c++

--- project3b-2270/main.cpp
+++ project3b-2270/main.cpp
@@ -20,8 +20,11 @@ class SplayTree
 			
 		}
 		
+		//ZIG
 		node* L_Rotation(node* tmp_node)
 		{
+			cout<<"L_Rotation"<<endl;
+			
 			node* p = tmp_node->parent;
 			
 			p->leftChild = tmp_node->rightChild;
@@ -33,27 +36,36 @@ class SplayTree
 			tmp_node->parent = p->parent;
 			p->parent = tmp_node;
 			
+			display(tmp_node);
+			
 			return tmp_node;
 		}
 		
+		//ZAG
 		node* R_Rotation(node* tmp_node)
 		{
+			cout<<"R_Rotation"<<endl;
+			
 			node* p = tmp_node->parent;
 			
 			p->rightChild = tmp_node->leftChild;
-			
+
 			if(p->rightChild)
 				p->rightChild->parent = p;
 				
 			tmp_node->leftChild = p;
 			tmp_node->parent = p->parent;
 			p->parent = tmp_node;
+
+			display(tmp_node);
 			
 			return tmp_node;
 		}
 		
 		node* LL_Rotation(node* tmp_node)
 		{
+			cout<<"LL_Rotation"<<endl;
+			
 			node* p = tmp_node->parent;
 			node* gp = p->parent;
 			node* ggp = gp->parent;
@@ -91,22 +103,16 @@ class SplayTree
 			}
 			else
 			{
+				display(tmp_node);
+				
 				return tmp_node;
 			}
 		}
 		
-		void LR_Rotation()
-		{
-			
-		}
-		
-		void RL_Rotation()
-		{
-			
-		}
-		
 		node* RR_Rotation(node* tmp_node)
 		{
+			cout<<"RR_Rotation"<<endl;
+			
 			node* p = tmp_node->parent;
 			node* gp = p->parent;
 			node* ggp = gp->parent;
@@ -144,6 +150,8 @@ class SplayTree
 			}
 			else
 			{
+				display(tmp_node);
+				
 				return tmp_node;
 			}
 		}
@@ -159,12 +167,13 @@ class SplayTree
 				if(tmp_node == NULL || tmp_node == root)
 				{
 					cout<<"Splay not needed. Already root."<<endl;
+					//return tmp_node;
 					break;
 				}
 				else
 				{
 					cout<<"Splaying node."<<endl;
-
+						
 					p = tmp_node->parent;
 
 					gp = p->parent;
@@ -174,12 +183,16 @@ class SplayTree
 						if(tmp_node == p->leftChild)
 						{
 							//Zig rotation
-							root = L_Rotation(tmp_node);
+							//cout<<"L_ROT"<<endl;
+							tmp_node = L_Rotation(tmp_node);
+							//display(root);
 						}
 						else
 						{
 							//Zag rotation
-							root = R_Rotation(tmp_node);
+							//cout<<"R_ROT"<<endl;
+							tmp_node = R_Rotation(tmp_node);
+							//display(root);
 						}
 					}
 					else
@@ -189,27 +202,43 @@ class SplayTree
 							if(tmp_node == p->leftChild)
 							{
 								//Zig-Zig rotation
-								root = LL_Rotation(tmp_node);
+								//cout<<"LL_ROT"<<endl;
+								tmp_node = LL_Rotation(tmp_node);
+								//display(root);
 							}
 							else
 							{
-								//Zig-Zag rotation
-								root = R_Rotation(tmp_node);
-								root = L_Rotation(tmp_node);
+								//Zag-Zig rotation
+								//cout<<"rl_ROT"<<endl;
+								//tmp_node = R_Rotation(tmp_node);
+								//cout<<"rl_ROT 11111111"<<endl;
+								tmp_node = L_Rotation(R_Rotation(tmp_node));
+								//cout<<"rl_ROT 22222222"<<endl;
+								//cout<<tmp_node->key<<" <tmp : root> "<< root->key<<endl;
+								//display(root);
 							}
 						}
 						else
 						{
 							if(tmp_node == p->leftChild)
 							{
-								//Zag-Zig rotation
-								root = L_Rotation(tmp_node);
-								root = R_Rotation(tmp_node);
+							
+								
+								//Zig-Zag rotation
+								
+								//tmp_node = L_Rotation(tmp_node);
+								tmp_node = R_Rotation(L_Rotation(tmp_node));
+								
+								
+								
+								//display(root);
 							}
 							else
 							{
 								//Zag-Zag rotation
-								root = RR_Rotation(tmp_node);
+								//cout<<"RR_ROT"<<endl;
+								tmp_node = RR_Rotation(tmp_node);
+								//display(root);
 							}
 						}
 					}
@@ -243,7 +272,7 @@ class SplayTree
 			//Inserting into empty tree.
 			if(!root)
 			{
-				cout<<"Placing node."<<endl;
+				//cout<<"Placing node."<<endl;
 				root = tmp_node;
 				tmp_node = NULL;
 				
@@ -260,11 +289,11 @@ class SplayTree
 					if(tmp_root->leftChild)
 					{
 						tmp_root = tmp_root->leftChild;
-						cout<<"Going deeper and left."<<endl;
+						//cout<<"Going deeper and left."<<endl;
 					}
 					else
 					{
-						cout<<"Placing node."<<endl;
+						//cout<<"Placing node."<<endl;
 						tmp_root->leftChild = tmp_node;
 						tmp_node->parent = tmp_root;
 						tmp_root = NULL;
@@ -277,11 +306,11 @@ class SplayTree
 					if(tmp_root->rightChild)
 					{
 						tmp_root = tmp_root->rightChild;
-						cout<<"Going deeper and right."<<endl;
+						//cout<<"Going deeper and right."<<endl;
 					}
 					else
 					{
-						cout<<"Placing node."<<endl;
+						//cout<<"Placing node."<<endl;
 						tmp_root->rightChild = tmp_node;
 						tmp_node->parent = tmp_root;
 						tmp_root = NULL;
@@ -291,7 +320,7 @@ class SplayTree
 				
 				if(tmp_node->key == tmp_root->key)
 				{
-					cout<<"Node is a duplicate."<<endl;
+					//cout<<"Node is a duplicate."<<endl;
 					tmp_node = tmp_root;
 					tmp_root = NULL;
 					break;
@@ -366,12 +395,12 @@ class SplayTree
 				if(height >= 1)
 				{
 					if(root->leftChild == NULL )
-						left = "***";
+						left = "...";
 					else
 						left = root->leftChild->key;
 					
 					if(root->rightChild == NULL)
-						right = "***";
+						right = "...";
 					else
 						right = root->rightChild->key;
 						
@@ -381,24 +410,24 @@ class SplayTree
 				if(height >= 2)
 				{
 					if(root->leftChild == NULL || root->leftChild->leftChild == NULL )
-						left = "***";
+						left = "...";
 					else
 						left = root->leftChild->leftChild->key;
 					
 					if(root->leftChild == NULL || root->leftChild->rightChild == NULL)
-						right = "***";
+						right = "...";
 					else
 						right = root->leftChild->rightChild->key;
 					
 					cout << "       "<<left << "             " << right;
 
 					if(root->rightChild == NULL || root->rightChild->leftChild == NULL )
-						left = "***";
+						left = "...";
 					else
 						left = root->rightChild->leftChild->key;
 					
 					if(root->rightChild == NULL || root->rightChild->rightChild == NULL)
-						right = "***";
+						right = "...";
 					else
 						right = root->rightChild->rightChild->key;
 
@@ -408,12 +437,12 @@ class SplayTree
 				if(height >= 3)
 				{
 					if(root->leftChild == NULL || root->leftChild->leftChild == NULL || root->leftChild->leftChild->leftChild == NULL)
-						left = "***";
+						left = "...";
 					else
 						left = root->leftChild->leftChild->leftChild->key;
 					
 					if(root->leftChild == NULL || root->leftChild->leftChild == NULL || root->leftChild->leftChild->rightChild == NULL)
-						right = "***";
+						right = "...";
 					else
 						right = root->leftChild->leftChild->rightChild->key;
 					
@@ -422,12 +451,12 @@ class SplayTree
 					
 					
 					if(root->leftChild == NULL || root->leftChild->rightChild == NULL || root->leftChild->rightChild->leftChild == NULL)
-						left = "***";
+						left = "...";
 					else
 						left = root->leftChild->rightChild->leftChild->key;
 					
 					if(root->leftChild == NULL || root->leftChild->rightChild == NULL || root->leftChild->rightChild->rightChild == NULL)
-						right = "***";
+						right = "...";
 					else
 						right = root->leftChild->rightChild->rightChild->key;
 					
@@ -435,12 +464,12 @@ class SplayTree
 					
 
 					if(root->rightChild == NULL || root->rightChild->leftChild == NULL || root->rightChild->leftChild->leftChild == NULL)
-						left = "***";
+						left = "...";
 					else
 						left = root->rightChild->leftChild->leftChild->key;
 					
 					if(root->rightChild == NULL || root->rightChild->leftChild == NULL || root->rightChild->leftChild->rightChild == NULL)
-						right = "***";
+						right = "...";
 					else
 						right = root->rightChild->leftChild->rightChild->key;
 
@@ -448,17 +477,148 @@ class SplayTree
 					
 					
 					if(root->rightChild == NULL || root->rightChild->rightChild == NULL || root->rightChild->rightChild->leftChild == NULL)
-						left = "***";
+						left = "...";
 					else
 						left = root->rightChild->rightChild->leftChild->key;
 					
 					if(root->rightChild == NULL || root->rightChild->rightChild == NULL || root->rightChild->rightChild->rightChild == NULL)
-						right = "***";
+						right = "...";
 					else
 						right = root->rightChild->rightChild->rightChild->key;
 					
 					cout << "     "<<left << "     " << right<<endl;
 				}
+				
+				if(height >= 4)
+				{
+					if(root->leftChild == NULL || root->leftChild->leftChild == NULL || root->leftChild->leftChild->leftChild == NULL || root->leftChild->leftChild->leftChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->leftChild->leftChild->leftChild->leftChild->key;
+					
+					if(root->leftChild == NULL || root->leftChild->leftChild == NULL || root->leftChild->leftChild->leftChild == NULL || root->leftChild->leftChild->leftChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->leftChild->leftChild->leftChild->rightChild->key;
+					
+					cout << " "<<left << " " << right;
+					
+					
+					
+					
+					if(root->leftChild == NULL || root->leftChild->leftChild == NULL || root->leftChild->leftChild->rightChild == NULL || root->leftChild->leftChild->rightChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->leftChild->leftChild->rightChild->leftChild->key;
+					
+					if(root->leftChild == NULL || root->leftChild->leftChild == NULL || root->leftChild->leftChild->rightChild == NULL || root->leftChild->leftChild->rightChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->leftChild->leftChild->rightChild->rightChild->key;
+					
+					cout << " "<<left << " " << right;
+					
+					
+					
+					
+					if(root->leftChild == NULL || root->leftChild->rightChild == NULL || root->leftChild->rightChild->leftChild == NULL || root->leftChild->rightChild->leftChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->leftChild->rightChild->leftChild->leftChild->key;
+					
+					if(root->leftChild == NULL || root->leftChild->rightChild == NULL || root->leftChild->rightChild->leftChild == NULL || root->leftChild->rightChild->leftChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->leftChild->rightChild->leftChild->rightChild->key;
+					
+					cout << " "<<left << " " << right;
+					
+					
+					
+					if(root->leftChild == NULL || root->leftChild->rightChild == NULL || root->leftChild->rightChild->rightChild == NULL || root->leftChild->rightChild->rightChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->leftChild->rightChild->rightChild->leftChild->key;
+					
+					if(root->leftChild == NULL || root->leftChild->rightChild == NULL || root->leftChild->rightChild->rightChild == NULL || root->leftChild->rightChild->rightChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->leftChild->rightChild->rightChild->rightChild->key;
+					
+					cout << " "<<left << " " << right;
+					
+					
+					
+					if(root->rightChild == NULL || root->rightChild->leftChild == NULL || root->rightChild->leftChild->leftChild == NULL || root->rightChild->leftChild->leftChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->rightChild->leftChild->leftChild->leftChild->key;
+					
+					if(root->rightChild == NULL || root->rightChild->leftChild == NULL || root->rightChild->leftChild->leftChild == NULL || root->rightChild->leftChild->leftChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->rightChild->leftChild->leftChild->rightChild->key;
+					
+					cout << " "<<left << " " << right;
+					
+					
+					
+					if(root->rightChild == NULL || root->rightChild->leftChild == NULL || root->rightChild->leftChild->rightChild == NULL || root->rightChild->leftChild->rightChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->rightChild->leftChild->rightChild->leftChild->key;
+					
+					if(root->rightChild == NULL || root->rightChild->leftChild == NULL || root->rightChild->leftChild->rightChild == NULL || root->rightChild->leftChild->rightChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->rightChild->leftChild->rightChild->rightChild->key;
+					
+					cout << " "<<left << " " << right;
+					
+					
+					
+					if(root->rightChild == NULL || root->rightChild->rightChild == NULL || root->rightChild->rightChild->leftChild == NULL || root->rightChild->rightChild->leftChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->rightChild->rightChild->leftChild->leftChild->key;
+					
+					if(root->rightChild == NULL || root->rightChild->rightChild == NULL || root->rightChild->rightChild->leftChild == NULL || root->rightChild->rightChild->leftChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->rightChild->rightChild->leftChild->rightChild->key;
+					
+					cout << " "<<left << " " << right;
+					
+					
+					
+					if(root->rightChild == NULL || root->rightChild->rightChild == NULL || root->rightChild->rightChild->rightChild == NULL || root->rightChild->rightChild->rightChild->leftChild == NULL) 
+						left = "...";
+					else
+						left = root->rightChild->rightChild->rightChild->leftChild->key;
+					
+					if(root->rightChild == NULL || root->rightChild->rightChild == NULL || root->rightChild->rightChild->rightChild == NULL || root->rightChild->rightChild->rightChild->rightChild == NULL) 
+						right = "...";
+					else
+						right = root->rightChild->rightChild->rightChild->rightChild->key;
+					
+					cout << " "<<left << " " << right<<endl;
+	
+					
+				}
+				
+				if(height >= 5)
+				{
+					cout<<"****************************************************************"<<endl;
+					cout<<"          Tree levels greater than 4 are not displyed.          "<<endl;
+					cout<<"****************************************************************"<<endl;
+				}
+			}
+			else
+			{
+				//Should never see this, but just incase.
+				cout<<"****************************************************************"<<endl;
+				cout<<"                  There is no tree to display.                  "<<endl;
+				cout<<"****************************************************************"<<endl;
 			}
 		}
 };
@@ -479,7 +639,6 @@ int main()
 	
 	while(1)
 	{
-		//system("cls");
 		cout << "\n ----------------------\n" << endl;
 		cout << " 1. Build tree from file." << endl;
 		cout << " 2. Insert a key." << endl;
@@ -536,7 +695,7 @@ int main()
 				root = splayTree.insertNode(key, root);
 				
 				//call display function here to show after every insert.
-				splayTree.display(root);
+				//splayTree.display(root);
 				
 				break;
 				
